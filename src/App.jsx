58d78e92a2351239aa97094e23230a6655db638f
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-08-06 19:16:02 +0530

    chore: rmv tru

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ function App() {
             Submit
           </button>
           {
-            true &&
+            isSubmitted &&
             <button
               onClick={() => ExportToXlxs(filteredContests)}
               className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -145,7 +145,7 @@ function App() {
         </form>
 
         {
-          true &&
+          isSubmitted &&
           <>
             {/* <button
               onClick={() => ExportToXlxs(filteredContests)}
