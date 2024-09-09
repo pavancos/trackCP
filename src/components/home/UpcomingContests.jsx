dc2be9bd3659e4841a7c587e503d62678956e61d
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-09-09 22:33:18 +0530

    feat:add features in landing page
    Co-authored-by: Vignesh Varanasi <varanasivsv@gmail.com>

--- src/components/home/UpcomingContests.jsx
+++ src/components/home/UpcomingContests.jsx
@@ -159,7 +159,7 @@ function UpcomingContests() {
 
 
     return (
-        <div className='upContests ms-10 scroll-section pt-12'>
+        <div className='upContests ms-10 scroll-section pt-12 mb-8'>
             <h1 className='text-5xl font-afacad font-medium mb-12'>Upcoming Contests</h1>
             <div ref={scrollRef} className="flex gap-4 overflow-x-auto py-4 rounded-xl scrollbar-hide">
                 <div className='flex gap-4 me-2'>
@@ -169,7 +169,7 @@ function UpcomingContests() {
                             :
                             upcoming.map((contest, index) => {
                                 return (
-                                    <div key={index} className="flex flex-col w-80 p-6 pb-0 sm:pb-4 sm:pe-4 bg-[#f5f5f5] rounded-28px hover:scale-[1.01] transition-all">
+                                    <div key={index} className="flex flex-col w-80 p-6 pb-4 sm:pe-4 bg-[#f5f5f5] rounded-28px hover:scale-[1.01] transition-all">
                                         <img src={contest.Image} alt="icon" className="w-8 mb-2" />
                                         <h2 className='text-2xl font-semibold mb-2'>{contest.platofrm}</h2>
                                         <p className="text-md">
