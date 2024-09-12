4227388247714258b3020ba328a0020d11344f54
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-09-12 22:26:15 +0530

    feat: add new batches fetches
    Co-authored-by: Vignesh Varanasi <varanasivsv@gmail.com>

--- src/functions/fetchFromDB/fetchFromDB.jsx
+++ src/functions/fetchFromDB/fetchFromDB.jsx
@@ -1,9 +1,27 @@
+// Get Student Info Data
 async function fetchFromDB(setstudentsInfo) {
-    let studentsDataFromAPI = await fetch(`https://getdata-contests.vercel.app/getAllData`);
-    let studentsData = await studentsDataFromAPI.json();
-    console.log('studentsData: ', studentsData);
-    setstudentsInfo(studentsData);
-    // console.log("useEffect Invoked");
-  }
+  let studentsDataFromAPI = await fetch(`https://getdata-contests.vercel.app/getAllData`);
+  let studentsData = await studentsDataFromAPI.json();
+  console.log('studentsData: ', studentsData);
+  setstudentsInfo(studentsData);
+  // console.log("useEffect Invoked");
+}
 
-export {fetchFromDB};
+// Getting Data of Batch 21
+async function fetch21BatchData(setBatch21Data){
+  let response= await fetch(`https://getdata-contests.vercel.app/getBatch21Data`);
+  let data = await response.json();
+  console.log("Batch 21 Data: ",data);
+  setBatch21Data(data);
+}
+
+// Getting Data of Batch 22
+async function fetch22BatchData(setBatch22Data) {
+  let response = await fetch(`https://getdata-contests.vercel.app/getBatch22Data`);
+  let data = await response.json();
+  console.log("Batch 22 data: ",data);
+  setBatch22Data(data);
+}
+
+
+export { fetchFromDB, fetch21BatchData, fetch22BatchData };
