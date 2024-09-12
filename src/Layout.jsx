3544b503d6c2d419b96e2f58a1e3202036411945
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-09-12 22:29:10 +0530

    feat: back-to-top button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,10 +3,39 @@ import Navbar from './components/navbar/Navbar';
 import { Outlet } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import BackToTop from './assets/Top.svg';
 
 function Layout() {
     const location = useLocation();
-    console.log('location: ', location);
+    // console.log('location: ', location);
+
+    const [showButton, setShowButton] = useState(false);
+  
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+  
+    // Scroll to top when navigating to a new page
+    useEffect(() => {
+      window.scrollTo({ top: 0, behavior: "auto" });
+    }, [location.pathname]);
+  
+    const handleScrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+  
     const isHomePage = location.pathname === '/';
     return (
         <div>
@@ -23,6 +52,12 @@ function Layout() {
                 !isHomePage &&
                 <Footer />
             }
+            <button
+                onClick={handleScrollToTop}
+                className={`back-to-top ${showButton ? "show" : "hide"}`}
+            >
+                <img src={BackToTop} width={37} alt="Back to Top" />
+            </button>
         </div>
     )
 }
