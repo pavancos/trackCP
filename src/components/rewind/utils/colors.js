836060dac3b86f7915c2b049d3786aab53a36962
pavancos <pavankc005@gmail.com>
2024-12-22 22:17:21 +0530

    feat: added more gradients

---
+++ src/components/rewind/utils/colors.js
@@ -0,0 +1,69 @@
+let colors = [
+    {
+        from: "#e2c8e5",
+        via: "#ffc1b3",
+        to: "#ffdcce"
+    }, {
+        from: "#5C8374",
+        via: "#a0b2af",
+        to: "#bac5c5"
+    }, {
+        from: "#Ac8e2b2",
+        via: "#f0eb8e",
+        to: "#fff3b3"
+    }, {
+        from: "#94fffb",
+        via: "#88e6e7",
+        to: "#ace6e9"
+    }
+    , {
+        from: "#e6cf9c",
+        via: "#f8d586",
+        to: "#fbe4b2"
+    }
+    , {
+        from: "#f3f9f9",
+        via: "#60abb3",
+        to: "#4da1a9"
+    }
+    , {
+        from: "#ffffff",
+        via: "#b5a9fa",
+        to: "#a294f9"
+    }
+    , {
+        from: "#fdf6ed",
+        via: "#f1c083",
+        to: "#f1bc79"
+    }
+    , {
+        from: "#f9fff2",
+        via: "#e1ffbb",
+        to: "#e1febb"
+    }
+    , {
+        from: "#fefefe",
+        via: "#bbcbdb",
+        to: "#d1e2f5"
+    }
+    , {
+        from: "#fef9fb",
+        via: "#fecbe0",
+        to: "#ffcce1"
+    }
+    , {
+        from: "#f9fefb",
+        via: "#a3eebe",
+        to: "#a0edbc"
+    }
+    , {
+        from: "#fefaf5",
+        via: "#ffbd73",
+        to: "#febd73"
+    }
+    , {
+        from: "#fefffe",
+        via: "#e1fce3",
+        to: "#e0fbe2"
+    }
+]
